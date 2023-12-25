39e73627ca85c422c5cd17eeb7248701cbb86166
SrinivasBallari <sps21042003@gmail.com>
2023-12-25 21:02:47 +0530

    setup sequelize

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const {PORT} = require('./config/serverConfig');
 
 async function setupAndStartServer(){
+
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    
     app.listen(PORT, () => {
         console.log(`server started on ${PORT}`);
     });
